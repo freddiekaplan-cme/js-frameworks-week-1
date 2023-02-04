aac2d7da94d6458a486e1bd3c1271955e54aa5fb
Freddie CME <freddie.kaplan@edu.cmeducations.se>
2023-02-04 23:01:15 +0100

    Added different styling to the articles and changing text of button

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,23 @@ import billboard from './assets/billboard.webp'
 import './App.css'
 
 function getActiveClassName (activeArticle, currentArticle) {
-  if (activeArticle === currentArticle) {
-    return 'show-article'
-  } else {
-    return 'hide-article'
+  if (activeArticle === currentArticle && activeArticle === 'First Article') {
+    return 'show-article, article'
   }
+  else if (activeArticle === currentArticle && activeArticle === 'Second Article') {
+     return 'show-article, second-article'
+   }
+   else if (activeArticle === currentArticle && activeArticle === 'Third Article') {
+     return 'show-article, third-article'
+   } else {
+     return 'hide-article'
+    }
 }
 
 function App() {
   const [activeArticle, setActiveArticle] = useState('First Article')
+
+  const [buttonText, setButtonText] = useState("Next Article: My Project");
 
   return (
     <>
@@ -21,8 +29,9 @@ function App() {
           <h1>React Week 1</h1>
         </header>
         <main>
+          <div className="buttons">
 
-        <div className="buttons">
+        {/*
             <button onClick={() => {
               setActiveArticle((a) => {
                 if (a === 'First Article') {
@@ -38,7 +47,38 @@ function App() {
                 }
               })
             }}>Switch Article</button>
-              {/* <button onClick={() => {
+          */}
+      
+            <button onClick={() => {
+
+              setActiveArticle((a) => {
+                if (a === 'First Article') {
+                  setButtonText("Next Article: Project Design")
+                  return 'Second Article'
+                }
+                else if (a === 'Second Article') {
+                  setButtonText("Next Article: About Me")
+                  return 'Third Article'
+                }
+                else if (a === 'Third Article') {
+                    setButtonText("Next Article: My Project")
+                    return 'First Article'
+                } else {
+                  setButtonText("Next Article: My Project")
+                  return 'First Article'
+                }
+              })
+            }}>
+              
+              {buttonText}
+
+            </button>
+
+             {/*  
+            <button onClick={() => { 
+                  setMyText("My Changed Text")}}>
+               
+                </button>
                 
                 getActiveArticle(() => {
                 return "show-article"
@@ -47,12 +87,12 @@ function App() {
           }}>All Articles</button> */}
           </div>
 
-          <article className={`article, first-article, ${getActiveClassName(activeArticle, 'First Article')}`} >
+          <article className={`${getActiveClassName(activeArticle, 'First Article')}`} >
              <h2>About Me</h2>
              <p>I'm learning web development. So far I've worked with HTML5 CSS3, JavaScript and jQuery. Excited to get into React and other js frameworks.</p>
              <img className='article-pic' src={japan} />
            </article>
-           <article className={`article, second-article, ${getActiveClassName(activeArticle, 'Second Article')}`} >
+           <article className={`${getActiveClassName(activeArticle, 'Second Article')}`} >
              <h2>Project for Week 1</h2>
              <p>My first personal project is a name generator. Click a buttton and it will give you a random name from two-three arrays.</p>
              <div className="figures">
@@ -61,7 +101,7 @@ function App() {
               <div className="figure">= Random Name</div>
              </div>
            </article>
-           <article className={`article, third-article, ${getActiveClassName(activeArticle, 'Third Article')}`} >
+           <article className={`${getActiveClassName(activeArticle, 'Third Article')}`} >
              <h2>Design</h2>
              <p>I picked <a href="https://dribbble.com/shots/17117044-Free-Big-City-Billboard-Mockup">this design</a>. Mostly because I think I can pull it off and it alos suits the theme I'm going for with the name generator.</p>
              <img className='article-pic' src={billboard} />
